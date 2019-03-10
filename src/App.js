0e610f6c6856639b14d97945bd5d62bda03e5f5b
FCW <731033719@qq.com>
2019-03-10 22:54:52 +0800

    fix store index 文件

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React, { Component, Fragment } from 'react';
+import { Provider } from 'react-redux';
 import Header from './common/header';
+import store from './store';
 class App extends Component {
   render() {
     return (
-      <Fragment>
-        <Header />
-      </Fragment>
+      <Provider store={store}>
+        <Fragment>
+          <Header />
+        </Fragment>
+      </Provider>
     );
   }
 }
